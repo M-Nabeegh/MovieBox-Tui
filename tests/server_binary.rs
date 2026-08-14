01635b697ad5c7c4cff034808d9ad7e3b87f1e09
M-Nabeegh <151343422+M-Nabeegh@users.noreply.github.com>
2026-08-14 18:39:58 +0500

    test(server): require server feature for binary smoke test

--- tests/server_binary.rs
+++ tests/server_binary.rs
@@ -1,3 +1,7 @@
+#[cfg(not(feature = "server"))]
+compile_error!("server feature is required for the server_binary test");
+
+#[cfg(feature = "server")]
 #[test]
 fn server_binary_is_built_with_server_feature() {
     let path = assert_cmd::cargo::cargo_bin!("moviebox-server");
